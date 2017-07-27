ccff46563130c3d538795f08766a4245c2e3f32a
New-bottle <775712558@qq.com>
2017-07-27 23:40:42 +0800

    make dic and count occur times

--- makedic.py
+++ makedic.py
@@ -1,18 +1,39 @@
 import jieba
+import pandas as pd
 train_file = "training_new.csv"
 filepath = './comment_new/'
 
-dic = []
-for i in range(6000):
+bag = []
+for i in range(5000,6000):
     with open(filepath+'comment_'+str(i)+'.txt', 'r') as fopen:
         content = ''
         for eachline in fopen:
             content += eachline
         seg_list = jieba.lcut(content.strip(), cut_all=False)
-        dic.extend(seg_list)
+        bag.extend(seg_list)
+# get word bag for every comment
+punctuation = [",", ":", ";", ".", "'", '"', "?", "/", "-", "+", "&", "(", ")", "*",
+"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " "]
+cleanbag = []              
+for token in bag:
+    for punc in punctuation:
+        token = token.replace(punc, "")               
+    if token != "":
+        cleanbag.append(token)
+# remove the puncuations
 
+# (pd.Series(cleanbag).value_counts())
+dic = list(set(cleanbag))
+
+
+# get the union-set
 with open('dict.txt', 'w') as f:
-    dic = list(set(dic))
     for eachword in dic:
         f.writelines(eachword.encode('utf8')+'\n')
+    f.close()
+
+pd.set_option('display.max_rows', None)
+print (pd.Series(cleanbag).value_counts())
+with open('dict_count.txt', 'w') as f:
+    print >>f, str(pd.Series(cleanbag).value_counts())
     f.close()
