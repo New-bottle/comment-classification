9921453688c3f8979318ba48c272e6aa0dac6d0f
New-bottle <775712558@qq.com>
2017-07-27 09:43:08 +0800

    func : get test data set

--- readin.py
+++ readin.py
@@ -4,15 +4,20 @@ import numpy as np
 import sys, os
 import csv
 
-# return a 
+# return a list
+'''
+[
+    ['balabala', float]
+    ['balabala', float]
+    ...
+]
+'''
 def get_training_set():
     filepath = './comment_new/'
-    files = []
     ans = []
     with open('example.csv', 'rb') as csvfile:
         csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
-            print((row))
             fopen = open(filepath+row['id']+'.txt', 'r')
             content = ''
             for eachLine in fopen:
@@ -20,7 +25,22 @@ def get_training_set():
             ans.append([content,row['pred']])
     return ans
 
+def get_test_set():
+    filepath = './comment_new/'
+    ans = []
+    with open('training_new.csv', 'rb') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader:
+            print(row)
+            fopen = open(filepath+row['id']+'.txt', 'r')
+            content = ''
+            for eachLine in fopen:
+                content += eachLine
+            ans.append([content, row['pred']])
+    return ans            
 
 if __name__ == '__main__':
     ans = get_training_set()
     print(ans[0][1])
+    test = get_test_set()
+    print(test[0][1])
