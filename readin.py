6e62abe3fdb3ba6ac851ccabcfacdfb87726a9df
New-bottle <775712558@qq.com>
2017-07-27 10:07:48 +0800

    read original data into pk1

--- readin.py
+++ readin.py
@@ -4,6 +4,8 @@ import numpy as np
 import sys, os
 import csv
 
+train_file = 'training_origin.pk1'
+test_file = 'test_origin.pk1'
 # return a list
 '''
 [
@@ -31,7 +33,6 @@ def get_test_set():
     with open('training_new.csv', 'rb') as csvfile:
         csv_reader = csv.DictReader(csvfile)
         for row in csv_reader:
-            print(row)
             fopen = open(filepath+row['id']+'.txt', 'r')
             content = ''
             for eachLine in fopen:
@@ -40,7 +41,9 @@ def get_test_set():
     return ans            
 
 if __name__ == '__main__':
-    ans = get_training_set()
-    print(ans[0][1])
+    train = get_training_set()
     test = get_test_set()
-    print(test[0][1])
+    with open(train_file, 'wb') as f:
+        pickle.dump(train, f)
+    with open(test_file, 'wb') as f:
+        pickle.dump(test, f)
